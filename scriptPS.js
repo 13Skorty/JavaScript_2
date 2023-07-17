d2532ef45f17e39b976a630a1b2d04be4bedffb9
13Skorty <stas1989goncarov@gmail.com>
2023-07-17 21:06:47 +0300

    first commit

---
+++ scriptPS.js
@@ -0,0 +1,5 @@
+const name2 = "MacBook Pro 13"
+let price = 10000;
+console.log("Обрано", name2, "ціна за штуку", price, "кредитів" )
+let price2 = 20000
+console.log("Обрано", name2, "ціна за штуку", price2, "кредитів" )
